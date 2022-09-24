a8c5fd1a5449c174ab6293207f8b9e63863fcaed
Mehedi Hasan Pranto <109532837+pranto-9@users.noreply.github.com>
2022-09-24 20:31:25 +0600

    Add files via upload

---
+++ Lab 04/2.357_49_08.cpp
@@ -0,0 +1,15 @@
+// Codeforces Problem No - 281A
+// Problem Name - Word Capitalization
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char word[1000];
+        cin>>word;
+     word[0]=toupper(word[0]);
+         cout<<word<<endl;
+
+    return 0;
+}
