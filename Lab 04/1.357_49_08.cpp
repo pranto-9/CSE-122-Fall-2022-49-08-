0bee8c216086003712f06959ef405104f641cac0
Mehedi Hasan Pranto <109532837+pranto-9@users.noreply.github.com>
2022-09-24 20:26:44 +0600

    Add files via upload

---
+++ Lab 04/1.357_49_08.cpp
@@ -0,0 +1,30 @@
+// Codeforces Problem No - 339A
+// Problem Name - Helpful Maths
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+
+    char num[100],temp[100];
+    cin>>num;
+    int j, i, len=strlen(num);
+
+    for(i=0; i<len-1; i+=2)
+    {
+        for(j=0; j<len-1; j+=2)
+        {
+            if(num[j]>num[j+2])
+            {
+                temp[j]=num[j];
+                num[j]=num[j+2];
+                num[j+2]=temp[j];
+            }
+        }
+    }
+    cout<<num<<endl;
+
+    return 0;
+}
